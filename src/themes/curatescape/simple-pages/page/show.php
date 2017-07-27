df1f3dea2a57456c29100011993bcdb571e592bc
enlore <n.e.lorenson@gmail.com>
2017-07-27 14:18:54 -0500

    Simple page template markup

--- src/themes/curatescape/simple-pages/page/show.php
+++ src/themes/curatescape/simple-pages/page/show.php
@@ -2,25 +2,19 @@
 $bodyclass = 'page simple-page show';
 echo head(array('maptype'=>'none','title' => html_escape(metadata('simple_pages_page', 'title')), 'bodyclass' => $bodyclass, 'bodyid' => html_escape(metadata('simple_pages_page', 'slug')))); ?>
 
-<div id="content">
-<article class="page show">
-<h2 class="instapaper_title"><?php echo metadata('simple_pages_page', 'title'); ?></h2>
+<article class="page section">
+	<div id="primary" class="container" role="main">
+        <h2 class="instapaper_title"><?php echo metadata('simple_pages_page', 'title'); ?></h2>
 
-
-	<div id="primary" class="show" role="main">
-		<section id="text">
-
+		<section class="page-body">
 		    <?php
 		    $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
 		    echo $this->shortcodes($text);
 		    ?>					    
-
 		</section>    
+
 	</div>
-
-
 </article>
-</div> <!-- end content -->
 
 <div id="share-this" class="browse">
 <?php echo mh_share_this();?>
