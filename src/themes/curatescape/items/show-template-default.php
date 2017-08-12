bb2bc9792b96ece1bef676d63af98d06da2fff5a
enlore <n.e.lorenson@gmail.com>
2017-08-12 17:24:52 -0500

    Clean up

--- src/themes/curatescape/items/show-template-default.php
+++ src/themes/curatescape/items/show-template-default.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $dc = get_theme_option('dropcap')==1 ? 'dropcap' : null;
 echo head(array(
     'item'=>$item,
@@ -14,17 +14,9 @@ echo head(array(
 
 <div id="content">
 
-<?php
-//var_dump($item->Files);
-//echo link_to($item->Files[0]);
-//echo mh_single_file_show($item->Files[0]);
-//echo img($item->Files[0]);
-//echo file_display_url($item->Files[0])
-?>
-
 <article class="item instapaper_body hentry" role="main">
-			
-	<header class="item-header">
+
+    <header class="item-header">
         <div class="item-headerImage"
             style="background-image: url(<?php echo file_display_url($item->Files[0]) ?>)">
 
@@ -35,7 +27,7 @@ echo head(array(
                     </span>
                 </h2>
             </div>
-            
+
         </div>
 
         <!--
@@ -43,64 +35,64 @@ echo head(array(
             <?php //echo mh_the_subtitle($item); ?>
         </h3>
         -->
-        
+
         <?php //echo mh_the_byline($item,true,true); echo item_is_private($item);?>
-	</header>
+    </header>
 
     <section class="item-metadata section instapaper_ignore">
         <div class="f-h3 item-legend"> Info </div>
 
         <div class="item-fieldset container">
-            <aside id="factoid">  	
+            <aside id="factoid">
             <?php echo mh_factoid(); ?>
-            </aside>	
+            </aside>
 
-            <div id="access-info">  	
+            <div id="access-info">
             <?php echo mh_the_access_information(); ?>
-            </div>	
+            </div>
 
             <div id="street-address">
-            <?php echo mh_street_address();?>	
+            <?php echo mh_street_address();?>
             </div>
-            
+
             <div id="official-website">
-            <?php echo mh_official_website();?>	
+            <?php echo mh_official_website();?>
             </div>
 
             <div id="cite-this">
             <?php echo mh_item_citation(); ?>
-            </div>	
-            
+            </div>
+
             <?php if(function_exists('tours_for_item')){
                  $label=mh_tour_label_option('plural');
-                 echo tours_for_item($item->id, __('Related %s', $label)); 
+                 echo tours_for_item($item->id, __('Related %s', $label));
             }?>
-                
-            <div id="subjects">  	
+
+            <div id="subjects">
             <?php mh_subjects(); ?>
-            </div>	
-            
-            <div id="tags">
-            <?php mh_tags();?>	
             </div>
-            
-            <?php echo function_exists('tour_nav') ? tour_nav(null,mh_tour_label()) : null; ?>		
+
+            <div id="tags">
+            <?php mh_tags();?>
+            </div>
+
+            <?php echo function_exists('tour_nav') ? tour_nav(null,mh_tour_label()) : null; ?>
 
             <div class="item-related-links">
             <?php mh_related_links();?>
             </div>
-            
+
             <div class="date-stamp">
-            <?php echo mh_post_date(); ?>				
+            <?php echo mh_post_date(); ?>
             </div>
-            
+
             <div class="comments">
             <?php mh_display_comments();?>
             </div>
         </div>
-    </section>	
+    </section>
 
-    <section class="item-copy section" style="padding-top: 0px;"> 
+    <section class="item-copy section" style="padding-top: 0px;">
         <div class="container">
             <div class="item-map">
                 <h2 class="f-upper" style="margin-bottom: 24px;"> Where can I find it? </h2>
@@ -110,7 +102,7 @@ echo head(array(
             <div class="item-lede">
                 <?php echo mh_the_lede($item);?>
             </div>
-                
+
             <div class="item-description">
                 <h2 class="f-upper" style="margin-bottom: 24px"> Description </h2>
                 <?php echo mh_the_text($item); ?>
@@ -120,35 +112,35 @@ echo head(array(
 
     <section class="item-media section">
         <div class="container">
-            <?php mh_item_images($item);?>	
-            
-            <?php mh_audio_files($item);?>		
-                    
+            <?php mh_item_images($item);?>
+
+            <?php mh_audio_files($item);?>
+
             <?php mh_video_files($item);?>
         </div>
     </section>
-            
+
 
     <div id="share-this" class="instapaper_ignore">
         <?php echo mh_share_this(mh_item_label());?>
-    </div>	
+    </div>
 
 </article>
 
 </div> <!-- end content -->
 
 <script>
-	
-	if(jQuery('.tour-nav').length > 0){
-		jQuery(window).scroll(function() {
-			if( (jQuery('.meta').isOnScreen() || jQuery('footer.main').isOnScreen()) !== false) {
-				jQuery('.tour-nav').addClass('look-at-me');
-			}else{
-				jQuery('.tour-nav').removeClass('look-at-me');		
-			}
-		}).scroll();
-	}
-	
+
+    if(jQuery('.tour-nav').length > 0){
+        jQuery(window).scroll(function() {
+            if( (jQuery('.meta').isOnScreen() || jQuery('footer.main').isOnScreen()) !== false) {
+                jQuery('.tour-nav').addClass('look-at-me');
+            }else{
+                jQuery('.tour-nav').removeClass('look-at-me');
+            }
+        }).scroll();
+    }
+
 </script>
 
 <?php echo foot(); ?>
