f7772d26d208b0f196809921312dbe3f11633f7b
enlore <n.e.lorenson@gmail.com>
2017-08-20 15:45:45 -0500

    Tags page for exhibits

--- src/themes/curatescape/exhibit-builder/exhibits/tags.php
+++ src/themes/curatescape/exhibit-builder/exhibits/tags.php
@@ -3,18 +3,13 @@ $title = __('Browse Exhibits by Tag');
 echo head(array('maptype'=>'none','title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'exhibits tags browse'));
 ?>
 
-
-<div id="content">
-
-<section class="browse tags">	
+<section class="allTagsScreen section">		
 	<h2><?php 
 	$title .= ( (isset($total_results)) ? ': <span class="item-number">'.$total_results.'</span>' : '');
 	echo $title; 
 	?></h2>
 
-
 	<div id="primary" class="browse">
-
 		<nav class="secondary-nav" id="item-browse">
 	    <?php echo nav(array(
 	            array(
@@ -29,13 +24,19 @@ echo head(array('maptype'=>'none','title' => $title, 'bodyid' => 'exhibit', 'bod
 	    ); ?>
     	</nav>
 		
-		<?php echo tag_cloud($tags, 'exhibits/browse'); ?>
+		<?php //echo tag_cloud($tags, 'exhibits/browse'); ?>
+
+        <div class="allTagsList">
+        <?php foreach ($tags as $tag) {
+            $_href = url('/exhibits/browse', array('tags' => $tag['name']));
+
+            $href = html_escape($_href);
+
+            echo '<a class="allTagsList-tag" href="' . $href . '">' . $tag['name'] . '</a>';
+        }?>
+        </div>
 	</div><!-- end primary -->
-
-
-
 </section>
-</div> <!-- end content -->
 
 <div id="share-this" class="browse">
 <?php echo mh_share_this();?>
