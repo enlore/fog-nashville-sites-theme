807c49d0231a1eaf6446bba64e937943a348f5b4
enlore <n.e.lorenson@gmail.com>
2017-08-27 18:47:02 -0500

    404 copy, link, other options

--- src/themes/curatescape/error/404.php
+++ src/themes/curatescape/error/404.php
@@ -1,17 +1,28 @@
 <?php 
 echo head(array('maptype'=>'focusarea','title'=>'404','bodyid'=>'error','bodyclass'=>'error_404')); ?>
-<?php mh_map_actions();?>
-<div id="content">
-<article class="error show">
-<h2>404</h2>
+<?php //mh_map_actions();?>
 
 
-	<div id="primary" class="show">
-		<section id="text">
-		    <p><?php echo __('Sorry. The page you are looking for does not exist!');?></p>
-		</section>
-	</div>
+<?php
+$msg = 'It looks like we’ve made a mistake! Please press your browser’s back'
+    . ' button to return to the previous page, or go <a class="f-anchor" href="/">Home</a>.';
+?>
 
-</article>
-</div> <!-- end content -->
+<section class="hero">
+    <div class="hero-body">
+        <div class="container u-max800">
+            <h1>404</h1>
+            <h2> Page Not Found </h2>
+            <p><?php echo __($msg);?></p>
+        </div>
+    </div>
+<section>
+
+<section class="section">
+    <div class="container u-max800">
+        <h2>Or try a different path.</h2>
+        <?php echo mh_display_homepage_tours(2); ?>
+    </div>
+</section>
+
 <?php echo foot(); ?>
