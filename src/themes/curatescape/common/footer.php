4ba8c5e6c7842bf47500ec8430f1bef3df922f9f
enlore <n.e.lorenson@gmail.com>
2017-09-17 16:36:18 -0500

    Item show layout and proper ordering

--- src/themes/curatescape/common/footer.php
+++ src/themes/curatescape/common/footer.php
@@ -7,71 +7,73 @@
 
 </div><!--end wrap-->
 
-<footer class="footer columns is-desktop" style="margin-top: 0px;">
-	<div class="footer-brand column">
-		<?php echo link_to_home_page(mh_the_logo(), array('class'=>'home-link')); ?>
-	</div>
+<footer class="footer is-desktop">
+    <div class="columns">
+        <div class="footer-brand column">
+            <?php echo link_to_home_page(mh_the_logo(), array('class'=>'home-link')); ?>
+        </div>
 
-    <nav class="footer-nav column is-2-tablet">
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/tours/browse">Take a Tour</a></li>
-            <li><a href="/items/browse">Items</a></li>
-            <li><a href="/about">About</a></li>
-        </ul>
-    </nav>
+        <nav class="footer-nav column is-2-tablet">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/tours/browse">Take a Tour</a></li>
+                <li><a href="/items/browse">Items</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
 
-    <p class="footer-about column is-5-tablet">
-        The Metropolitan Historical Commission Foundation is a 501(c)(3) non-profit
-        friends group which assists the Metro Historical Commission in its efforts to
-        identify, protect, study, and interpret the rich history of Nashville.
-    </p>
+        <p class="footer-about column is-5-tablet">
+            The Metropolitan Historical Commission Foundation is a 501(c)(3) non-profit
+            friends group which assists the Metro Historical Commission in its efforts to
+            identify, protect, study, and interpret the rich history of Nashville.
+        </p>
 
-	<div class="footer-address column">
-		<address>
-			Sunnyside in Sevier Park<br/>
-			3000 Granny White Pike<br/>
-			Nashville, Tennesee 37204
-		</address>
+        <div class="footer-address column">
+            <address>
+                Sunnyside in Sevier Park<br/>
+                3000 Granny White Pike<br/>
+                Nashville, Tennesee 37204
+            </address>
 
-		<p> Phone: 615-862-7970 </p>
+            <p> Phone: 615-862-7970 </p>
 
-		<p> Copyright 2017. <br /> All rights reserved. </p>
+            <p> Copyright 2017. <br /> All rights reserved. </p>
 
-		<div class="footer-byline">
-            <div>Designed for this time</div>
-            <div>and place by</div>
-            <a href="http://fog.haus">
-                <img class="footer-fog" src="/themes/curatescape/images/fog-logo-white.png" alt="fog.haus"/>
-            </a>
-		</div>
-	</div>
+            <div class="footer-byline">
+                <div>Designed for this time</div>
+                <div>and place by</div>
+                <a href="http://fog.haus">
+                    <img class="footer-fog" src="/themes/curatescape/images/fog-logo-white.png" alt="fog.haus"/>
+                </a>
+            </div>
+        </div>
 
-	<!--<nav id="footer-nav">    
-	    <?php echo mh_global_nav(); ?>      	
-    	 <div id="search-wrap">
-	    	<?php echo mh_simple_search($formProperties=array('id'=>'footer-search')); ?>
-	    </div>   
-    </nav>	
-	<p class="default">
-		  <span id="app-store-links"><?php mh_appstore_footer(); ?></span>  
-		<?php echo mh_footer_find_us();?>
-		<span id="copyright"><?php echo mh_license();?></span> 
-		<span id="powered-by"><?php echo __('Powered by <a href="http://omeka.org/">Omeka</a> + <a href="http://curatescape.org">Curatescape</a>');?></span>
-	</p>
-	<div class="custom">
-		<?php echo get_theme_option('custom_footer_html');?>
-	</div> -->
+        <!--<nav id="footer-nav">    
+            <?php echo mh_global_nav(); ?>      	
+             <div id="search-wrap">
+                <?php echo mh_simple_search($formProperties=array('id'=>'footer-search')); ?>
+            </div>   
+        </nav>	
+        <p class="default">
+              <span id="app-store-links"><?php mh_appstore_footer(); ?></span>  
+            <?php echo mh_footer_find_us();?>
+            <span id="copyright"><?php echo mh_license();?></span> 
+            <span id="powered-by"><?php echo __('Powered by <a href="http://omeka.org/">Omeka</a> + <a href="http://curatescape.org">Curatescape</a>');?></span>
+        </p>
+        <div class="custom">
+            <?php echo get_theme_option('custom_footer_html');?>
+        </div> -->
 
-	<?php 		
-		echo mh_footer_scripts_init(); 
-	?>
-	
-	
-	<!-- Plugin Stuff -->
-	<?php echo fire_plugin_hook('public_footer', array('view'=>$this)); ?>	
-		
-<!-- <div class="clearfix"></div> -->
+    <?php 		
+    echo mh_footer_scripts_init(); 
+    ?>
+
+
+        <!-- Plugin Stuff -->
+        <?php echo fire_plugin_hook('public_footer', array('view'=>$this)); ?>	
+
+    <!-- <div class="clearfix"></div> -->
+    </div>
 </footer>
 </body>
 
--- src/themes/curatescape/items/show-template-default.php
+++ src/themes/curatescape/items/show-template-default.php
@@ -26,136 +26,197 @@ echo head(array(
         <?php //echo mh_the_byline($item,true,true); echo item_is_private($item);?>
     </header>
 
-    <div class="columns is-desktop columns-is-col">
-        <div class="column">
-            <section class="item-copy section" style="padding-top: 0px;">
-                    <div class="item-description">
-                        <h2 class="f-upper f-body" style="margin-bottom: 24px"> Description </h2>
-                        <?php echo mh_the_text($item); ?>
-                    </div>
-
-                    <div class="item-map">
-                        <h2 class="f-upper f-body" style="margin-bottom: 24px;"> Where can I find it? </h2>
-                        <?php echo mh_display_map('story', $item) ?>
-                        <?php mh_map_actions($item,null);?>
-
-                    </div>
-
-                    <div class="item-lede">
-                        <?php echo mh_the_lede($item);?>
-                    </div>
+    <div class="item-body float-container">
+        <div class="float-right-desktop width-50-desktop pad-lr-desktop">
+            <section class="item-headerImage"
+                style="background-image: url(<?php echo file_display_url($item->Files[0]) ?>)">
             </section>
 
-            <section class="item-media section">
-                <div class="columns flex-wrap">
-                    <?php //mh_item_images($item);?>
-                    <?php foreach($item->Files as $file):
-                        $fileUrl = file_display_url($file);
-                        $fileTitle = metadata($file, array('Dublin Core', 'Title'));
-                        $fileDesc = metadata($file, array('Dublin Core', 'Description'));
-                        $fileHref = record_url($file, 'show');
-                        $fileElement = '';
-                        $fileElement
-                            .= '<div class="column is-half-desktop">'
-                                .'<a href="'. $fileHref .'">'
-                                    . '<div class="item-mediaImage" style="background-image: url('. $fileUrl .')">'
-                                    . '</div>'
-
-                                    . '<div class="item-mediaTitle f-h4">'
-                                        . $fileTitle
-                                    . '</div>'
-
-                                    . '<div class="item-mediaDescription">'
-                                        . $fileDesc
-                                    . '</div>'
-                                . '</a>'
-                            . '</div>'
-                            ;
-
-                        echo $fileElement;
-                    endforeach; ?>
-
-                    <?php mh_audio_files($item);?>
-
-                    <?php mh_video_files($item);?>
-                </div>
-            </section>
-
-
-            <div id="share-this" class="instapaper_ignore">
-                <?php echo mh_share_this(mh_item_label());?>
-            </div>
-
-        </div>
-
-        <div class="column item-order-1-mobile">
-            <section class="item-metadata section instapaper_ignore">
-                <div class="item-headerImage"
-                    style="background-image: url(<?php echo file_display_url($item->Files[0]) ?>)">
-                </div>
-
-                <div class="f-h3 item-legend"> Info </div>
+            <section class="item-metadata none block-desktop instapaper_ignore">
+                <div class="f-h2 item-legend"> Info </div>
 
                 <div class="item-fieldset">
-                    <aside id="factoid">
-                    <?php echo mh_factoid(); ?>
-                    </aside>
+                    <div id="factoid">
+                        <?php echo mh_factoid(); ?>
+                    </div>
 
                     <div id="access-info">
-                    <?php echo mh_the_access_information(); ?>
+                        <?php echo mh_the_access_information(); ?>
                     </div>
 
                     <div id="street-address">
-                    <?php echo mh_street_address();?>
+                        <?php echo mh_street_address();?>
                     </div>
 
                     <div id="official-website">
-                    <?php echo mh_official_website();?>
+                        <?php echo mh_official_website();?>
                     </div>
 
                     <div id="cite-this">
-                    <?php echo mh_item_citation(); ?>
+                        <h3> <?php echo __('Cite this Page:'); ?> </h3>
+
+                        <?php echo html_entity_decode(metadata('item', 'citation'));?>
                     </div>
 
                     <?php if(function_exists('tours_for_item')){
-                         $label=mh_tour_label_option('plural');
-                         echo tours_for_item($item->id, __('Related %s', $label));
+                        $label=mh_tour_label_option('plural');
+                        echo tours_for_item($item->id, __('Related %s', $label));
                     }?>
 
                     <div id="subjects">
-                    <?php mh_subjects(); ?>
+                        <?php mh_subjects(); ?>
                     </div>
 
                     <?php echo function_exists('tour_nav') ? tour_nav(null,mh_tour_label()) : null; ?>
 
                     <div class="item-related-links">
-                    <?php mh_related_links();?>
+                        <?php mh_related_links();?>
                     </div>
 
                     <div class="date-stamp">
-                    <?php echo mh_post_date(); ?>
+                        <?php echo mh_post_date(); ?>
                     </div>
 
                     <div class="comments">
-                    <?php mh_display_comments();?>
+                        <?php //mh_display_comments();?>
                     </div>
                 </div>
+            </section>
 
+            <section class="item-tags none block-desktop pad-bot-desktop">
+                <?php //mh_tags();?>
+                <?php if (metadata($item, 'has tags') ): ?>
+                    <?php $tags=tag_string(get_current_record('item') , url('items/browse'), ''); ?>
+                    <h2 class="item-tagsTitle f-body f-upper"> <?php echo __('Tags');?> </h2>
 
-                <div class="item-tags">
-                    <?php //mh_tags();?>
-                    <?php if (metadata($item, 'has tags') ): ?>
-                        <?php $tags=tag_string(get_current_record('item') , url('items/browse'), ''); ?>
-                        <h2 class="item-tagsTitle f-body f-upper"> <?php echo __('Tags');?> </h2>
-
-                        <div class="item-tagsList f-h4">
-                            <?php echo $tags; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-
+                    <div class="item-tagsList f-h4">
+                        <?php echo $tags; ?>
+                    </div>
+                <?php endif; ?>
             </section>
         </div>
+
+
+        <!--
+        <section class="item-lede width-50-desktop">
+            <?php //echo mh_the_lede($item);?>
+        </section>
+        -->
+        <section class="item-description">
+            <h2 class="f-upper f-body" style="margin-bottom: 24px"> Description </h2>
+            <?php echo mh_the_text($item); ?>
+        </section>
+
+        <section class="item-map float-clear-both">
+            <h2 class="f-upper f-body" style="margin-bottom: 24px;"> Where can I find it? </h2>
+            <?php echo mh_display_map('story', $item) ?>
+            <?php mh_map_actions($item,null);?>
+        </section>
+
+        <section class="item-metadata none-desktop instapaper_ignore">
+            <div class="f-h2 item-legend"> Info </div>
+
+            <div class="item-fieldset">
+                <aside id="factoid">
+                <?php echo mh_factoid(); ?>
+                </aside>
+
+                <div id="access-info">
+                <?php echo mh_the_access_information(); ?>
+                </div>
+
+                <div id="street-address">
+                <?php echo mh_street_address();?>
+                </div>
+
+                <div id="official-website">
+                <?php echo mh_official_website();?>
+                </div>
+
+                <div id="cite-this">
+                <?php echo mh_item_citation(); ?>
+                </div>
+
+                <?php if(function_exists('tours_for_item')){
+                    $label=mh_tour_label_option('plural');
+                    echo tours_for_item($item->id, __('Related %s', $label));
+                }?>
+
+                <div id="subjects">
+                <?php mh_subjects(); ?>
+                </div>
+
+                <?php echo function_exists('tour_nav') ? tour_nav(null,mh_tour_label()) : null; ?>
+
+                <div class="item-related-links">
+                <?php mh_related_links();?>
+                </div>
+
+                <div class="date-stamp">
+                <?php echo mh_post_date(); ?>
+                </div>
+
+                <div class="comments">
+                <?php mh_display_comments();?>
+                </div>
+            </div>
+        </section>
+
+        <section class="item-tags none-desktop">
+            <?php //mh_tags();?>
+            <?php if (metadata($item, 'has tags') ): ?>
+                <?php $tags=tag_string(get_current_record('item') , url('items/browse'), ''); ?>
+                <h2 class="item-tagsTitle f-body f-upper"> <?php echo __('Tags');?> </h2>
+
+                <div class="item-tagsList f-h4">
+                    <?php echo $tags; ?>
+                </div>
+            <?php endif; ?>
+        </section>
+
+        <section class="item-media">
+            <h2 class="f-body f-upper"> Take a Look </h2>
+
+            <div class="item-mediaGallery columns flex-wrap">
+                <?php //mh_item_images($item);?>
+                <?php foreach($item->Files as $file):
+                    $fileUrl = file_display_url($file);
+                $fileTitle = metadata($file, array('Dublin Core', 'Title'));
+                $fileDesc = metadata($file, array('Dublin Core', 'Description'));
+                $fileHref = record_url($file, 'show');
+                $fileElement = '';
+                $fileElement
+                    .= '<div class="column is-6 is-4-desktop">'
+                    .'<a href="'. $fileHref .'">'
+                    . '<div class="item-mediaImage" style="background-image: url('. $fileUrl .')">'
+                    . '</div>'
+
+                    . '<div class="item-mediaTitle f-h4">'
+                    . $fileTitle
+                    . '</div>'
+
+                    . '<div class="item-mediaDescription">'
+                    . $fileDesc
+                    . '</div>'
+                    . '</a>'
+                    . '</div>'
+                    ;
+
+                echo $fileElement;
+                endforeach; ?>
+
+                <?php mh_audio_files($item);?>
+
+                <?php mh_video_files($item);?>
+            </div>
+        </section>
+
+
+        <section id="share-this" class="instapaper_ignore width-50-desktop">
+            <?php echo mh_share_this('Page');?>
+        </section>
+
+        <div class="float-clear"></div>
     </div>
 </article>
 
