5407d29c136d42897fe25a1a09dc4dab811aa313
enlore <n.e.lorenson@gmail.com>
2017-09-19 23:14:18 -0500

    Conditional meta echo

--- src/themes/curatescape/items/show-template-default.php
+++ src/themes/curatescape/items/show-template-default.php
@@ -8,6 +8,27 @@ echo head(array(
     'bodyclass'=>'show item-story '.$dc,
     'title' => metadata($item,array('Dublin Core', 'Title'))
 ));
+$source = metadata('item'      , array('Dublin Core' , 'Source'));
+$publisher = metadata('item'   , array('Dublin Core' , 'Publisher'));
+$date = metadata('item'        , array('Dublin Core' , 'Date'));
+$contributor = metadata('item' , array('Dublin Core' , 'Contributor'));
+$rights = metadata('item'      , array('Dublin Core' , 'Rights'));
+$format = metadata('item'      , array('Dublin Core' , 'Format'));
+$relation = metadata('item'    , array('Dublin Core' , 'Relation'));
+$type = metadata('item'        , array('Dublin Core' , 'Type'));
+$language = metadata('item'    , array('Dublin Core' , 'Language'));
+$identifier = metadata('item'  , array('Dublin Core' , 'Identifier'));
+$coverage = metadata('item'    , array('Dublin Core' , 'Coverage'));
+//$cite = metadata('item', array('Dublin Core', 'Publisher'));
+function fog_echo_meta ($meta, $label) {
+    if ($meta) {
+        echo '<div class="dublin-core-meta-field">'
+            .'<h3>'. $label .'</h3>';
+            echo $meta;
+        echo '</div>';
+    }
+}
+
 ?>
 
 <article class="item instapaper_body hentry" role="main">
@@ -43,61 +64,30 @@ echo head(array(
                     <!--<div class="access-info">-->
                         <!--<?php echo mh_the_access_information(); ?>-->
                     <!--</div>-->
-                    
-                    <div class="source">
-                        <h3> Source </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Source'))?>
-                    </div>
 
-                    <div class="publisher">
-                        <h3> Publisher </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Publisher'))?>
-                    </div>
+                    <?php if ($source) { ?>
+                        <div class="source">
+                            <h3> Source </h3>
+                            <?php echo $source; ?>
+                        </div>
+                    <?php } ?>
 
-                    <div class="Date">
-                        <h3> Date </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Date'))?>
-                    </div>
+                    <?php if ($publisher) { ?>
+                        <div class="publisher">
+                            <h3> Publisher </h3>
+                            <?php echo $publisher; ?>
+                        </div>
+                    <?php } ?>
 
-                    <div class="contributor">
-                        <h3> Contributor </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Contributor'))?>
-                    </div>
-
-                    <div class="rights">
-                        <h3> Rights </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Rights'))?>
-                    </div>
-
-                    <div class="format">
-                        <h3> Format</h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Format'))?>
-                    </div>
-
-                    <div class="relation">
-                        <h3> Relation </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Relation'))?>
-                    </div>
-
-                    <div class="type">
-                        <h3> Type </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Type'))?>
-                    </div>
-
-                    <div class="language">
-                        <h3> Language </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Language'))?>
-                    </div>
-
-                    <div class="Identifier">
-                        <h3> Identifier</h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Identifier'))?>
-                    </div>
-
-                    <div class="coverage">
-                        <h3> Coverage </h3>
-                        <?php echo metadata('item', array('Dublin Core', 'Coverage'))?>
-                    </div>
+                    <?php fog_echo_meta($date, "Date"); ?>
+                    <?php fog_echo_meta($contributor, "Contributor"); ?>
+                    <?php fog_echo_meta($rights, "Rights"); ?>
+                    <?php fog_echo_meta($format, "Format"); ?>
+                    <?php fog_echo_meta($relation, "Relation"); ?>
+                    <?php fog_echo_meta($type, "Type"); ?>
+                    <?php fog_echo_meta($language, "Language"); ?>
+                    <?php fog_echo_meta($identifier, "Identifier"); ?>
+                    <?php fog_echo_meta($coverage, "Coverage"); ?>
 
                     <div class="street-address">
                         <?php echo mh_street_address();?>
@@ -131,9 +121,11 @@ echo head(array(
                         <?php echo mh_post_date(); ?>
                     </div>
 
+                    <!--
                     <div class="comments">
                         <?php //mh_display_comments();?>
                     </div>
+                    -->
                 </div>
             </section>
 
