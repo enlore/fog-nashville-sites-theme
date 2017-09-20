416976eed513c562687aecfbefe6c86037f7dfdf
enlore <n.e.lorenson@gmail.com>
2017-09-19 22:53:05 -0500

    Fix layout on tours

--- src/themes/curatescape/tour-builder/tours/browse.php
+++ src/themes/curatescape/tour-builder/tours/browse.php
@@ -5,16 +5,17 @@ echo head( array('maptype'=>'none', 'title' => $label, 'bodyid'=>'tours',
 ?>
     <section class="section browseToursSection">			
 
-        <header>
-            <h2><?php echo __('All %1$s: %2$s', $label, total_tours());?></h2>
-        </header>
-
         <div id="primary" class="container">
-            <div id="results">
+
+            <header>
+                <h2><?php echo __('All %1$s: %2$s', $label, total_tours());?></h2>
+            </header>
+
                 <nav class="tours-nav navigation secondary-nav">
                   <?php echo public_nav_tours(); ?>
                 </nav>	
 
+            <div id="results">
                 <div class="pagination bottom"><?php echo pagination_links(); ?></div>
 
                 <?php 
@@ -29,26 +30,24 @@ echo head( array('maptype'=>'none', 'title' => $label, 'bodyid'=>'tours',
                         $tourdesc = strip_tags( htmlspecialchars_decode(tour( 'description' )) );
                         $imgSrc = fog_get_image_url_of_first_public_item($tour);
                     
-                        echo '<article id="item-result-'.$i.'" class="browseTour columns is-tablet is-desktop">';
-                            echo '<div class="column">';
-                                echo '<a href="'. record_url($tour, 'show') .'">';
-                                    echo '<h3 class="browseTour-title">'.$tour->title.'</h3>';
-                                    echo '<div class="browseTour-imageBg" style="background-image: url(' . $imgSrc . ')">' . '</div>';
-                                echo '</a>';
+                        echo '<article id="item-result-'.$i.'" class="browseTour">';
+                            echo '<a href="'. record_url($tour, 'show') .'">';
+                                echo '<h3 class="browseTour-title">'.$tour->title.'</h3>';
+                                echo '<div class="browseTour-imageBg" style="background-image: url(' . $imgSrc . ')">' . '</div>';
+                            echo '</a>';
 
-                                echo '<div class="browseTour-meta">';
-                                    if(tour( 'Credits' )){
-                                        echo __('%1s curated by: %2s', mh_tour_label_option('singular'),tour( 'Credits' )).' | ';
-                                    }elseif(get_theme_option('show_author') == true){
-                                        echo __('%1s curated by: The %2s Team',mh_tour_label_option('singular'),option('site_title')).' | ';
-                                    }		
+                            echo '<div class="browseTour-meta">';
+                                if(tour( 'Credits' )){
+                                    echo __('%1s curated by: %2s', mh_tour_label_option('singular'),tour( 'Credits' )).' | ';
+                                }elseif(get_theme_option('show_author') == true){
+                                    echo __('%1s curated by: The %2s Team',mh_tour_label_option('singular'),option('site_title')).' | ';
+                                }		
 
-                                    echo count($tour->Items).' '.__('Locations').
-                                '</div>';
-                            echo '</div>';
+                                echo count($tour->Items).' '.__('Locations').
+                            '</div>';
                         
 
-                            echo '<div class="browseTour-description column"><p>'.snippet($tourdesc,0,250).'</p></div>'; 
+                            echo '<div class="browseTour-description"><p>'.snippet($tourdesc,0,250).'</p></div>'; 
                         echo '</article>';
                         $i++;
                     
